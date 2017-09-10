a97d4c4692d5845aa2836c22f11b6c9317d33808
aminghafari <amin.ghafari@berkeley.edu>
2017-09-09 18:41:07 -0700

    restore the model

--- hw1/sec2_batch.py
+++ hw1/sec2_batch.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pickle
 import tensorflow as tf
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import math
 
-def model(x,hD3):
+def model(x,out_D):
 	# setup variables
 	hD1 = 40
 	hD2 = 40
+	hD3 = 20
 	# layer1
 	W1 = tf.get_variable("W1", shape = [x.shape[1], hD1])
 	b1 = tf.get_variable("b1", shape = [1, hD1])
@@ -17,17 +18,20 @@ def model(x,hD3):
 	# layer3
 	W3 = tf.get_variable('W3', shape = [hD2, hD3])
 	b3 = tf.get_variable('b3', shape = [1, hD3])
+	# layer4
+	W4 = tf.get_variable('W4', shape = [hD3, out_D])
+	b4 = tf.get_variable('b4', shape = [1, out_D])
 	# graph
-	h1 = tf.nn.elu( tf.matmul(x, W1) + b1 )
-	h2 = tf.nn.elu( tf.matmul(h1, W2) + b2 )
-	y_out = tf.matmul(h2, W3) + b3
+	h1 = tf.nn.relu( tf.matmul(x,  W1) + b1 )
+	h2 = tf.nn.relu( tf.matmul(h1, W2) + b2 )
+	h3 = tf.nn.relu( tf.matmul(h2, W3) + b3 )
+	y_out = tf.matmul(h3, W4) + b4
 
 	return y_out
 
 
 def main():
 	import argparse
-
 	parser = argparse.ArgumentParser()
 	parser.add_argument('expert_data',type=str)
 	parser.add_argument('envname', type=str)
@@ -63,8 +67,7 @@ def main():
 	
 	batch_size = 100
 	losses = []
-	for epoch in range(10):
-		print('epoch:',epoch)
+	for epoch in range(40):
 		for i in range(int(math.ceil(N/batch_size))):
 			start_idx = i*batch_size%N
 			idx = train_indicies[start_idx:start_idx+batch_size]
@@ -76,46 +79,17 @@ def main():
 			loss_step,_ = sess.run([loss, train], feed_dict=feed_dict)
 			
 			losses.append(loss_step)
+		print('epoch:',epoch,', mean_loss:',loss_step)
 			#print(loss_step)
 
-	#plt.plot(losses)
-	#plt.ylabel('loss')
-	#plt.xlabel('iterations of batch size = 100, 5 epochs, data size = 20000 ')
-	#plt.show()
+	saver = tf.train.Saver()
+	saver.save(sess, args.envname+"-trained")
 
-	# run the trained policy on data
-	import gym
-	env = gym.make(args.envname)
-	max_steps = env.spec.timestep_limit
-	print(max_steps)
-	num_rollouts = 20
+	plt.plot(losses)
+	plt.ylabel('loss')
+	plt.xlabel('iterations of batch size = 100, 5 epochs, data size = 20000 ')
+	plt.show()
 
-	returns = []
-	observations = []
-	actions = []
-	for i in range(num_rollouts):
-		print('iter', i)
-		obs = env.reset()
-		done = False
-		totalr = 0.
-		steps = 0
-		while not done:
-			action = sess.run(y_out, feed_dict={x : np.array([obs])})
-			observations.append(obs)
-			actions.append(action)
-			obs, r, done, _ = env.step(action)
-			totalr += r
-			steps += 1
-			
-			env.render()
-			if steps % 100 == 0: print("%i/%i"%(steps, max_steps))
-			if steps >= max_steps:
-				break
-		returns.append(totalr)
-
-	print('returns', returns)
-	print('mean return', np.mean(returns))
-	print('std of return', np.std(returns))
 
 if __name__ == '__main__':
     main()
---
+++ hw1/sec3.py
@@ -0,0 +1,62 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import math
+
+
+def main():
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('trained_model',type=str)
+	parser.add_argument('envname', type=str)
+	args = parser.parse_args()
+
+
+	# retrieve the session
+	with tf.Session() as sess:
+		new_saver = tf.train.import_meta_graph(args.trained_model)
+		new_saver.restore(sess, tf.train.latest_checkpoint('./'))
+
+		# import the graph and the output
+		graph = tf.get_default_graph()
+		x = graph.get_tensor_by_name("x:0")
+		y_out = graph.get_tensor_by_name("y_out:0")
+	
+		# run the trained policy on data
+		import gym
+		env = gym.make(args.envname)
+		max_steps = env.spec.timestep_limit
+		print(max_steps)
+		num_rollouts = 1
+
+		returns = []
+		observations = []
+		actions = []
+		for i in range(num_rollouts):
+			print('iter', i)
+			obs = env.reset()
+			done = False
+			totalr = 0.
+			steps = 0
+			while not done:
+				action = sess.run('y_out', feed_dict={x : np.array([obs])})
+				observations.append(obs)
+				actions.append(action)
+				obs, r, done, _ = env.step(action)
+				totalr += r
+				steps += 1
+				
+				env.render()
+				if steps % 100 == 0: print("%i/%i"%(steps, max_steps))
+				if steps >= max_steps:
+					break
+			returns.append(totalr)
+	
+		#returns_expert = expert_data['returns']
+		print('mean return:', np.mean(returns))
+		#print('mean return for expert:', np.mean(returns_expert))
+		print('std of return', np.std(returns))
+		#print('std of return for expert', np.std(returns_expert))
+	
+if __name__ == '__main__':
+    main()
